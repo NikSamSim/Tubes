250abdd08c0d257848902b8f4db96571b3c0d79c
Gixgine-budi <miqbalr001@gmail.com>
2024-11-04 14:52:58 +0700

    Update tubes-rev.py

---
+++ tubes-rev.py
@@ -0,0 +1,57 @@
+# Bikin AC
+
+#function
+def swing:
+	pass
+
+def fan:
+	pass
+
+def mode:
+	pass
+
+def turbo:
+	pass
+
+def quiet:
+	pass
+
+def sleep:
+	pass
+
+def timer:
+	pass
+
+def atur_suhu:
+	pass
+
+def auto:
+	pass
+
+#Main program
+power = 'off'
+pilihan = ''
+
+while(True):
+	power = input("Masukkan power: ")
+	if(power == 'on'):
+		pilihan = input("Mau masukin apa: ")
+		match pilihan:
+			case 'swing':
+
+			case 'fan':
+
+			case 'mode':
+
+			case "turbo":
+
+			case 'quiet':
+
+			case 'sleep':
+
+			case 'timer':
+
+			case 'atur suhu':
+
+			case "auto":
+				
