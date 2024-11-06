fae08b23fc538f9c58a75730c2235837fb697f7c
gixgine-budi <miqbalr001@gmail.com>
2024-11-06 12:27:45 +0700

    changing from cli based to gui based

--- tubes-rev.py
+++ tubes-rev.py
@@ -1,6 +1,12 @@
 # Bikin AC
 
+import tkinter as tk
+import tkinter.messagebox
+import tkinter.ttk
+import sys
+
 #function
+#TODO define the functions
 def swing():
 	pass
 
@@ -22,44 +28,112 @@ def sleep():
 def timer():
 	pass
 
-def atur_suhu():
-	pass
+def set_temperature_up():
+    global temperature
+    temperature += 1
+    temperature_text.set(f"{temperature}\N{DEGREE SIGN}")
+    
+def set_temperature_down():
+    global temperature
+    temperature -= 1
+    temperature_text.set(f"{temperature}\N{DEGREE SIGN}")
 
 def auto():
 	pass
 
 #Main program
-power = 'off'
+power = False
 pilihan = ''
+temperature = 0
 
-print("hello world!")
 
-while(True):
-	power = input("Masukkan power: ")
-	if(power == 'on'):
-		pilihan = input("Mau masukin apa: ")
-		match pilihan:
-			case 'swing':
-				swing()
-			case 'fan':
-				fan()
-			case 'mode':
-				mode()
-			case "turbo":
-				turbo()
-			case 'quiet':
-				quiet()
-			case 'sleep':
-				sleep()
-			case 'timer':
-				timer()
-			case 'atur suhu':
-				atur_suhu()
-			case "auto":
-				auto()
-			case _:
-				print("Wrong input!")
-	else:
-		print("Power off.")
-		break
-     
+# initializing window
+root = tk.Tk()
+frm_top = tk.Frame(root)
+frm_top.grid()
+
+
+# Displaying temperature and such
+#TODO make the temperature display to automatically update
+frm1 = tk.Frame(frm_top, height=500, width=500, bg="red")
+frm1.grid(row=0, column=0)
+
+temperature_display = tk.Frame(frm_top, height=500, width=500, bg = "green")
+temperature_display.grid(row=0, column=1)
+
+
+temperature_text = tk.StringVar()
+temperature_text.set(f"{temperature}\N{DEGREE SIGN}")
+
+temperature_number = tk.ttk.Label(temperature_display, text=temperature_text.get())
+temperature_number.grid(row=0,column=0)
+temperature_number.configure(font=("Calibri", 100))
+
+#buttons
+frm_button = tk.Frame(root, bg="magenta")
+frm_button.grid()
+
+
+def switch_power():
+    global power
+    power = not power
+    
+power_button = tk.ttk.Button(frm_button, text="POWER", command=switch_power)
+power_button.grid(row=0, column=0)
+
+swing_button = tk.ttk.Button(frm_button, text="SWING", command=swing)
+swing_button.grid(row=0, column=1)
+
+fan_button = tk.ttk.Button(frm_button, text="FAN", command=fan)
+fan_button.grid(row=1, column=0)
+
+mode_button = tk.ttk.Button(frm_button, text="MODE", command=mode)
+mode_button.grid(row=1, column=1)
+
+turbo_button = tk.ttk.Button(frm_button, text="TURBO", command=turbo)
+turbo_button.grid(row=2, column=0)
+
+quiet_button = tk.ttk.Button(frm_button, text="QUIET", command=quiet)
+quiet_button.grid(row=2, column=1)
+
+sleep_button = tk.ttk.Button(frm_button, text="SLEEP", command=sleep)
+sleep_button.grid(row=3, column=0)
+
+timer_button = tk.ttk.Button(frm_button, text="TIMER", command=timer)
+timer_button.grid(row=3, column=1)
+
+temperature_up_button = tk.ttk.Button(frm_button, text="UP", command=set_temperature_up)
+temperature_up_button.grid(row=4, column=0)
+
+temperature_down_button = tk.ttk.Button(frm_button, text="DOWN", command=set_temperature_down)
+temperature_down_button.grid(row=4, column=1)
+
+# while(True):
+# 	if(power == True):
+# 		print("power on.")
+# 		pilihan = input("Mau masukin apa: ")
+# 		match pilihan:
+# 			case 'swing':
+# 				swing()
+# 			case 'fan':
+# 				fan()
+# 			case 'mode':
+# 				mode()
+# 			case "turbo":
+# 				turbo()
+# 			case 'quiet':
+# 				quiet()
+# 			case 'sleep':
+# 				sleep()
+# 			case 'timer':
+# 				timer()
+# 			case 'atur suhu':
+# 				atur_suhu()
+# 			case "auto":
+# 				auto()
+# 			case _:
+# 				print("Wrong input!")
+# 	else:
+# 		print("Power off.")
+
+root.mainloop()
